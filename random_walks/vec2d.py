f0dc9198ef1716abff9fd42743466d674da84e25
Oscar Arambula <peteroso876@gmail.com>
2025-02-16 15:18:12 -0600

    feat: correlated random walk with wrapped cauchy distribution

--- random_walks/vec2d.py
+++ random_walks/vec2d.py
@@ -3,11 +3,11 @@ import numpy.typing as npt
 import functools
 
 
-def rotate(v: npt.NDArray, radians: float) -> npt.NDArray:
+def rotate(vector: npt.NDArray, radians: float) -> npt.NDArray:
     """
     Rotate vector v by angle in radians.
     """
-    return v.dot(get_rotation_matrix(radians))
+    return vector.dot(get_rotation_matrix(radians))
 
 
 @functools.lru_cache(maxsize=4)
@@ -17,7 +17,9 @@ def get_rotation_matrix(radians: float) -> npt.NDArray:
     """
     cos_theta, sin_theta = np.cos(radians), np.sin(radians)
 
-    return np.array([
-        [cos_theta, -sin_theta],
-        [sin_theta, cos_theta],
-    ])
+    return np.array(
+        [
+            [cos_theta, -sin_theta],
+            [sin_theta, cos_theta],
+        ]
+    )
